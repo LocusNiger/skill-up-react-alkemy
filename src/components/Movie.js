01ee7516a448c615afd34fffc86a88188cd63463
Iván Sánchez <sanchezivan.798@gmail.com>
2022-07-18 17:13:37 -0300

    Cambios visuales

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,6 @@
 import { Link } from "react-router-dom";
 
 export default function Movie(props) {
-  /* formateando fecha */
-  let mm = props.date.substring(5, 7);
-  let aa = props.date.substring(0, 4);
   return (
     <>
       <div className="inline-block relative text-center items-center w-72 h-max box-border ">
@@ -12,12 +9,13 @@ export default function Movie(props) {
           src={`https://image.tmdb.org/t/p/w500/${props.img}`}
           alt={`${props.title} poster`}
         />
-        <div className="absolute inset-x-0 bottom-0 backdrop-blur-md h-32 mt-4 text-xl font-bold text-white  flex flex-col justify-evenly font-sans">
-          <p>{props.title}</p>
-          <p>{`${mm}/${aa}`}</p>
-          <p>⭐ {props.rate.toFixed(1)}</p>
+        <div className="absolute inset-x-0 bottom-0 backdrop-blur-sm h-32 mt-4 text-xl font-bold text-white  flex flex-col justify-evenly">
+          <p className="font-medium uppercase">{props.title}</p>
+          <p className="font-light">⭐ {props.rate.toFixed(1)}</p>
           {/* Botón de detalle guarda en la ruta el id de c/película */}
-          <Link to={`/detail?movieId=${props.id}`}>View detail</Link>
+          <Link to={`/detail?movieId=${props.id}`} className="font-light">
+            View detail
+          </Link>
         </div>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        oswald: ["Oswald", "sans-serif"],
+      },
       colors: {
         killBill: "#f8ba00",
         midnight: "#121721",
